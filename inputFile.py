c9c0914c78e6a34fe83b6e4b74d75f13e88cbf16
Linn Eriksson <linn@Linns-MacBook-Pro.local>
2025-06-15 16:26:25 -0400

    Updated to the parameters of the Nominal simulation

--- inputFile.py
+++ inputFile.py
@@ -23,11 +23,11 @@ Rstar  = 3.096*c.R_sun.cgs.value
 Tstar = 4397
 
 # Protoplanetary disk parameters
-Mdot0 = 1.0e-8*Msolar/year
+Mdot0 = 1.e-8*Msolar/year
                     # Initial disk accretion rate (determines initial
                     # gas surface density)
 Rout = 50.0*au      # Critical cut-off radius of surface density
-Z = 0.01
+Z = 0.01            # Initial dust-to-gas ratio
 tempProfile = "passIrr"
                     # Choose between CG97 or passIrr
 Tconst = 150.0      # Temperature at 1au in model CG97
@@ -35,13 +35,13 @@ tempExp = 0.5       # dlnT/dlnR
 sigmaExp = 1.0      # dlnSigma/dlnR at 1au
 HExp = 1.25         # dlnH/dlnR
 alpha = 5.0e-3      # alpha governing viscous evolution
-alphaTurb = 0.0001
+alphaTurb = 0.0001  # alpha governing turbulent diffusion
 mu = 2.34           # Mean molecular weight
 sig_h = 2e-15       # Collisional cross-section of hydrogen atom
 
 # Dust evolution parameters
-dustMinSize = 5e-5  # Minimum dust size on dust-grid in dustPy
-dustMaxSize = 100   # Maximum dust size on dust-grid in dustPy
+dustMinSize = 5e-6  # Minimum dust size on dust-grid in dustPy
+dustMaxSize = 10    # Maximum dust size on dust-grid in dustPy
 allowDriftingParticles = True
                     # Do or don't allow initially drifting particles in dustpy
 Rin_dust = 1.0*au   # Inner edge of semimajor axis grid used for dust ev.
@@ -49,7 +49,7 @@ Redge_dust = 200*au # Outer edge of semimajor axis grid used for dust ev.
 Rnr_dust = 150      # Nr of grid-points on semimajor axis grid used for dust ev.
 Rcoarse_int = 50*au # Coarse radial grid spacing in outer disk
 Rgrid_out = 1000*au # Outer edge of grid
-a_0 = 1.0e-4        # Initial size of dust grains
+a_0 = 1.0e-5        # Initial size of dust grains
 vfrag = 100.0       # Fragmentation velocity
 rhop = 1.0          # Internal density of dust grains
 
